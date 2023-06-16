88b2f249ed81c0ffa0022995435031bdcd80dfce
Dinesh Kumar G <gdineshda@gmail.com>
2023-06-16 14:49:54 +0530

    Likes

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	BaseUrl = "https://graph.facebook.com"
+	Version = "v17.0"
+	Endpoint = "likes"
+	AccessToken = "Your Access Token"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	pageId := "101372079670424_118057034648183"
+	url := fmt.Sprintf("%v/%v/%s/%v?since=%v&until=%v&access_token=%v", BaseUrl, Version, pageId, Endpoint, "2023-06-15", "2023-06-16", AccessToken);
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if (err != nil) {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(string(body))
 }
